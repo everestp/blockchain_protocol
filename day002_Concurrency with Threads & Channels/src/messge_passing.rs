15fc0fd57fc4016fbbb5a3da2cd25284364c11c1
everestp <paudeleverest09@gmail.com>
2025-10-08 11:44:15 +0545

    sum of 1..+400 using multithreading and message passing

--- day002_Concurrency with Threads & Channels/src/messge_passing.rs
+++ day002_Concurrency with Threads & Channels/src/messge_passing.rs
@@ -1,12 +1,38 @@
-use std::{sync::mpsc, thread};
+use std::{sync::mpsc, thread::{self, spawn}};
+//mpsc -> multiple prodiucer single consumer 
 
 fn main(){
-   let (tx ,rx)= mpsc::channel();
-    thread::spawn(move ||{
-        let val = String::from("Hi");
-        tx.send(val).unwrap();
-    });
-    
-    let recieved = rx.recv().unwrap();
-    println!("Got :{}",recieved);
+
+//find the sum for 1 to 1000
+let (tx ,rx)= mpsc::channel();
+let (tx1 ,rx1)= mpsc::channel();
+
+ thread::spawn( move || {
+    let mut sum1 = 0;
+for i in 1..=200{
+ sum1 = sum1+i;
+}
+tx.send(sum1).unwrap();
+ });
+
+ thread::spawn(move  || {
+    let mut sum2 = 0;
+    for i in 201..=400{
+  sum2 = sum2 + i;
+    }
+    tx1.send(sum2).unwrap();
+ });
+// let  mut total_sum = 0;
+// while let Ok(message) = rx.recv() {
+//      total_sum = total_sum + message;
+// }
+
+
+
+let recieved1 = rx1.recv().unwrap();
+println!("Total sum form 1 to 200: {}",recieved1);
+let recieved2 = rx.recv().unwrap();
+println!("Total sum form 200 to 401: {}",recieved2);
+ let total_sum = recieved1 + recieved2;
+println!("Total sum form 1 to 400: {}",total_sum);
 }
