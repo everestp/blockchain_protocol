44ad06c0080d1440d856d104329e3745d2f9ec91
everestp <paudeleverest09@gmail.com>
2025-10-09 14:54:26 +0545

    Understanding Async Programming in Rust

--- day_003_Async_Rust_with_Tokio/src/main.rs
+++ day_003_Async_Rust_with_Tokio/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
-}
+
+
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+ async  fn main() {
+
+println!("Starting node...");
+ fetch_block_data().await;
+  fetch_block_data().await;
+ println!("Node finidhed")
+
+ }
+
+
+ async  fn fetch_block_data(){
+    println!("Fetching block data");
+    sleep(Duration::from_secs(1)).await;      //simultate the network  delay
+    println!("Blocked data recieed")
+ }
