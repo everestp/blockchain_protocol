270034a188b6e6058f5d7276f58403b720401ac5
everestp <paudeleverest09@gmail.com>
2025-10-09 15:39:16 +0545

    Async HTTP Requests with Tokio and Reqwest

--- day_003_Async_Rust_with_Tokio/src/main.rs
+++ day_003_Async_Rust_with_Tokio/src/main.rs
@@ -1,14 +1,30 @@
 
 
+use reqwest::Client;
 use tokio::time::{sleep, Duration};
 
-#[tokio::main]
- async  fn main() {
 
+
+
+
+
+
+#[tokio::main]
+ async  fn main() ->Result<() , reqwest::Error> {
+    let client = Client::new();
+    
+    let response =  client
+    .get("https://jsonplaceholder.typicode.com/posts/1")
+    .send()
+    .await?
+    .text()
+    .await;
+   println!("Response: {}",response.unwrap() );
 println!("Starting node...");
  fetch_block_data().await;
   fetch_block_data().await;
- println!("Node finidhed")
+ println!("Node finidhed");
+ Ok(())
 
  }
 
@@ -18,3 +34,5 @@ println!("Starting node...");
     sleep(Duration::from_secs(1)).await;      //simultate the network  delay
     println!("Blocked data recieed")
  }
+
+
