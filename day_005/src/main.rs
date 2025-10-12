a06f47ecdbbf36c5b996b78e4e77208e0bc83419
everestp <paudeleverest09@gmail.com>
2025-10-12 14:12:44 +0545

    Basics of Serde

--- day_005/src/main.rs
+++ day_005/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+
+
+
+#[derive(Serialize ,Deserialize ,Debug)]
+struct Transaction{
+    id:u32,
+    amount:u64,
+    sender:String
+}
+
+
+
+fn main()->Result<() ,serde_json::Error>{
+    let tx= Transaction{id:1 ,amount:100 ,sender:String::from("Everest")};
+let serialized = serde_json::to_string(&tx)?;
+println!("Seralized Json {:?}",serialized);
+
+let deserialize:Transaction = serde_json::from_str(&serialized)?;
+println!("Deserialzed :{:?}",deserialize);
+assert_eq!(tx.sender ,deserialize.sender);
+
+
+
+    Ok(())
 }
