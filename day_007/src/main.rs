dbae316243a9f0c501f7aeac301f23d901269a7b
everestp <paudeleverest09@gmail.com>
2025-10-15 10:40:24 +0545

    Basics of Rust Testing:

--- day_007/src/main.rs
+++ day_007/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+fn  validate_transaction(amount :u64)->bool {
+    amount <=100
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_validate_transaction(){
+        assert!(validate_transaction(10));
+        assert!(validate_transaction(50));
+
+    }
 }
